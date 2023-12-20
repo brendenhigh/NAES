47a459b02e964b680b9b11a1d5ea3ce70c60aff7
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-12-20 15:41:19 -0500

    Update Home.php
    Added link for signup page.

--- Home.php
+++ Home.php
@@ -7,7 +7,7 @@
   <body>
     <h2>Home</h2>
     <p>Welcome to the NAES website. This is a website used to notify users of events pertaining to them.</p>
-    <p>If you would like to sign up, please use our signup page.</p>
+    <p>If you would like to sign up, please use our <a href="Signup.php">Signup</a> page.</p>
     <p>For users of this site use this link to get to the <a href="Login.php">Login</a> page.</p>
   </body>
 </html>
