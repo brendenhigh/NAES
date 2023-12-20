a6afcedc230b02113fda77a5ac931a1c832f20a1
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-12-20 14:18:15 -0500

    Create Home.php
    Added basic paragraphs and a link to the Login page.

---
+++ Home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Home</title>
+  </head>
+  <body>
+    <h2>Home</h2>
+    <p>Welcome to the NAES website. This is a website used to notify users of events pertaining to them.</p>
+    <p>If you would like to sign up, please use our signup page.</p>
+    <p>For users of this site use this link to get to the <a href="Login.php">Login</a> page.</p>
+  </body>
+</html>
