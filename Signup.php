8734b5c2a0b447b0f12858c50e1217a8a0328c0d
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-12-20 15:47:08 -0500

    Update Signup.php
    Added form to fill out name, username, password, and email address.

--- Signup.php
+++ Signup.php
@@ -5,6 +5,19 @@
   </head>
   <body>
     <h2>Signup</h2>
-    <p>To signup use this form to fill out your username, password, and full name.</p>
+    <p>To signup use this form to fill out your full name, email address, username, and password.</p>
+    <form method="post">
+      <label>First Name:</label>
+      <input type="text" name="First Name"><br>
+      <label>Last Name:</label>
+      <input type="text" name="Last Name"><br>
+      <label>Email Address:</label>
+      <input type="text" name="Email"><br>
+      <label>Username:</label>
+      <input type="text" name="Username"><br>
+      <label>Password:</label>
+      <input type="password" name="Password><br>
+      <button type="submit">Submit</button>
+    </form>
   </body>
 </html>
