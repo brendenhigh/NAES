848a6996722245accabc649491750561b88633c0
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-12-20 14:05:08 -0500

    Update Login.php
    Created a form for Login.

--- Login.php
+++ Login.php
@@ -4,6 +4,12 @@
     <title>Login</title>
   </head>
   <body>
-    
+    <form action="Login.php" method="post">
+      <label>Username:</label>
+      <input type="text" name="username"><br>
+      <label>Password:</label>
+      <input type="password" name="password"><br>
+      <button type="submit">Login</button>
+    </form>
   </body>
 </html>
