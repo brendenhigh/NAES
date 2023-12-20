205026d3801ef981503c61473fe6445cde96f8f1
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-12-20 14:22:49 -0500

    Create Signup.php
    Created beginning of signup page.

---
+++ Signup.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Signup</title>
+  </head>
+  <body>
+    <h2>Signup</h2>
+    <p>To signup use this form to fill out your username, password, and full name.</p>
+  </body>
+</html>
